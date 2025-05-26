e8ce8f714b647a0f8d0e252c25ee5ac4df09a1f1
cybugg <timehinsamuel11@gmail.com>
2025-05-26 12:39:41 +0300

    update

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -109,54 +109,20 @@ Only sign this message if you trust AgriEthos.
     <Image src={"/icons/logo.svg"} alt="logo" width={46} height={61.5} />
     <h2 className="text-3xl">Welcome to Agriethos</h2>
     <div className='italic'>Sign in to grow trust with every harvest.</div>
-    <div className='flex gap-2 text-xs text-grey-600'>
+    <div className='flex gap-2'>
       <div>
         Don&apos;t have an account? 
       </div>
-      <Link href={"/register"} className='text-primary-500 underline'>
+      <Link href={"/register"}>
         Register
       </Link>
     </div>
-    <div className='flex flex-col gap-3 w-full'>
- <button
+    <button
       onClick={connectWallet}
-      className="px-4 mt-5 w-full py-2 border  text-black rounded"
-    >
-      {loading && success !== "successful"?<Loader />: !loading && success === "success"?"Login successful": <div className='flex items-center justify-center gap-2'><Image src={"/icons/metamask.png"} alt="metamask" width={18} height={14} /><div>Connect with Metamask</div></div>}
-    </button>
-    <button
-      
-      className="px-4  w-full py-2 border boder-gray-500 text-black rounded"
-    >
-      {loading && success !== "successful"?<Loader />: !loading && success === "success"?"Login successful": <div className='flex items-center justify-center gap-1'><Image src={"/icons/google.svg"} alt="google" width={18} height={14} /><div>Sign in with Google</div></div>}
-    </button>
-    {/* Divider */}
-    <div className='  w-full py-2 flex items-center justify-between gap-3 text-black rounded'>
-<div className='w-full border border-gray-200'>
-
-</div>
-<div className='text-grey-500'>
-OR
-</div>
-<div className='w-full border border-gray-200'>
-
-</div>
-    </div>
-    {/* Email Auth */}
-    <div className=' w-full border boder-gray-500 text-black rounded'>
-<input type='email' className='w-full p-2 px-4 outline-none' placeholder='Email' required />
-    </div>
-    <div className=' flex gap-2 items-center  w-full  border boder-gray-500 text-black rounded'>
-<input type={'password'} className='w-full p-2 outline-none px-4 ' placeholder='Password' required />
-    </div>
-    <button
       className="px-4 mt-5 w-full py-2 bg-primary-600 text-black rounded"
     >
-      {loading && success !== "successful"?<Loader />: !loading && success === "success"?"Login successful": <div className='flex items-center justify-center gap-2'><div>Continue</div></div>}
+      {loading && success !== "successful"?<Loader />: !loading && success === "success"?"Login successful": <div className='flex items-center justify-center gap-1'><div>Connect with Metamask</div><Image src={"/icons/metamask.png"} alt="metamask" width={18} height={14} /></div>}
     </button>
-
-    </div>
-   
       </div> 
     {successSub && <Alert message='Logged in successful ... redirecting' color='text-green-800' background='bg-green-100' onClose={()=> setSuccessSub(false)}/>}
     {msg && <p className="text-red-600 mt-2">{msg}</p>}

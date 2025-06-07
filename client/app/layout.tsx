4cb62fd282f3c72d0d2ef92b0bde575099575d35
cybugg <timehinsamuel11@gmail.com>
2025-06-07 05:06:48 +0300

    update upgrade bug fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Agriethos",
-  description: "",
+  description: "Sustainably Grown, Globally Shared",
 };
 
 export default function RootLayout({
